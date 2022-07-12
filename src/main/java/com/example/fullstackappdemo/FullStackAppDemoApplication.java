310bdd8e5377a6f4676470bb4b8cf0042d3f6ac3
antoniu.pitic <antoniu.pitic@gmail.com>
2022-07-12 10:40:58 +0300

    Aplicatie care face nimic, dar ruleaza. Ar trebui sa contina toate dependintele de care avem nevoie

---
+++ src/main/java/com/example/fullstackappdemo/FullStackAppDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.fullstackappdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackAppDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullStackAppDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fullstackappdemo/FullStackAppDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fullstackappdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackAppDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
