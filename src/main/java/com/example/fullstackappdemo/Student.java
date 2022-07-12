e4e89fdf0af7549feabdffe1ebcb6de301d56148
antoniu.pitic <antoniu.pitic@gmail.com>
2022-07-12 10:53:41 +0300

    Am adaugat entitatea Student

---
+++ src/main/java/com/example/fullstackappdemo/Student.java
@@ -0,0 +1,58 @@
+package com.example.fullstackappdemo;
+
+import javax.persistence.*;
+
+@Entity
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private int id;
+
+    private String firstName;
+    private String surName;
+    private String mail;
+    private String pictureUrl;
+
+    public Student() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getPictureUrl() {
+        return pictureUrl;
+    }
+
+    public void setPictureUrl(String pictureUrl) {
+        this.pictureUrl = pictureUrl;
+    }
+}
